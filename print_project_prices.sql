4919f323da5bf7bca0ac68cf70b1e5dc968c18a6
Yevhenii Makovskyi <114361102+YujinPrime@users.noreply.github.com>
2023-04-20 19:20:35 +0300

    Update print_project_prices.sql

--- print_project_prices.sql
+++ print_project_prices.sql
@@ -2,4 +2,5 @@ SELECT p.name, SUM(salary)*DATEDIFF(MONTH, start_date, finish_date) AS price
 FROM project p
 JOIN project_worker ON p.id = project_worker.project_id
 JOIN worker ON project_worker.worker_id = worker.id
-GROUP BY p.name;
+GROUP BY p.name
+ORDER BY price DESC;
